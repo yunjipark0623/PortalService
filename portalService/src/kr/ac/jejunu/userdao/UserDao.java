61466e896ba5907103f4777eeae1ef9280d6b192
박윤지 <bearji0623@gmail.com>
2019-03-22 19:39:45 +0900

    190322

---
+++ portalService/src/kr/ac/jejunu/userdao/UserDao.java
@@ -0,0 +1,67 @@
+package kr.ac.jejunu.userdao;
+
+import java.sql.*;
+
+public class UserDao {
+    public User get(Long id) throws ClassNotFoundException, SQLException {
+        //DB 가 뭐야? mysql
+        //어딨어? 알려주께..
+        //드라이버 로드
+        Connection connection = getConnection();
+
+        //SQL 쿼리 만들고
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("select * from userinfo where id = ?");//statement를 안쓰고 prepareStatement를 이유는 쿼리 파싱을 안하기 때문에 파싱 타임을 줄여줌 왜냐면 계속 똑같은 쿼리문을 사용하기 때문에
+        preparedStatement.setLong(1, id);
+
+        //쿼리 실행하고
+        ResultSet resultSet = preparedStatement.executeQuery();//이때 쿼리문을 날리는 거임, 날리면 resultSet에 넣어주는 거임
+
+        //실행된 쿼리를 오브젝트에 매핑하고
+        resultSet.next();//커서를 다음거를 가르키게 해주는 거임, 우리가 명령을 통해서 옮겨줘야 함, DBMS구조상의 문제임
+        User user = new User();
+        user.setId(resultSet.getLong("id"));//resultSet에서 받아온다.
+        user.setName(resultSet.getString("name"));
+        user.setPassword(resultSet.getString("password"));
+
+        //자원 해지하고
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();//이것만 해줘도 다 날라가는데 하나하나씩 close하는 이유는 어떤것은 Open이 되있고 어떤것은 close되있으면 오류가 날 수도 있다. 일일히 close해주는게 좋다.
+
+        //리턴
+        return user;
+
+        //이 코드는 java를 가지고 mySQL에 접속하는 코드이다. 아직까지도 우리나라는 mySQL을 많이 사용하기 때문에 알아둬야 함
+    }
+
+    public Long add(User user) throws ClassNotFoundException, SQLException {
+        Connection connection = getConnection();
+
+        PreparedStatement preparedStatement =
+                connection.prepareStatement("insert into userinfo(name, password) values(?, ?)");
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getPassword());
+
+        preparedStatement.executeUpdate();
+
+        preparedStatement = connection.prepareStatement("select last_insert_id()");
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        resultSet.next();
+        Long id = resultSet.getLong(1);
+
+        resultSet.close();
+        preparedStatement.close();
+        connection.close();
+
+        return id;
+    }
+
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+
+        return DriverManager.getConnection("jdbc:mysql://172.18.102.128/jeju?serverTimezone=UTC"
+                , "portal", "portaljejunu");
+    }
+}
---
+++ portalService/src/kr/ac/jejunu/userdao/UserDaoTests.java
@@ -0,0 +1,38 @@
+package kr.ac.jejunu.userdao;
+
+import org.junit.Test;
+
+import java.sql.SQLException;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class UserDaoTests {
+    @Test
+    public void testGet() throws SQLException, ClassNotFoundException {
+        Long id = 1l;
+        String name = "허윤호";
+        String password = "1234";
+        UserDao userDao = new UserDao();
+        User user = userDao.get(id);
+        assertThat(user.getId(), is(id));//user.getId에서 받은 값이 id값이랑 같은것인지 is물어보는 것, 굉장히 직관적
+        assertThat(user.getName(), is(name));
+        assertThat(user.getPassword(), is(password));
+    }
+
+    @Test
+    public void testAdd() throws SQLException, ClassNotFoundException {
+        String name = "헐크";
+        String password = "1111";
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+        UserDao userDao = new UserDao();
+        Long id = userDao.add(user);
+        User resultUser = userDao.get(id);
+        assertThat(resultUser.getId(), is(id));//save한 것이 맞는지 is로 물어보는 것
+        assertThat(resultUser.getName(), is(name));
+        assertThat(resultUser.getPassword(), is(password));
+    }
+}
